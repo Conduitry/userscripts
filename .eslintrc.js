41fa36932a903eb8a88999172f019d9041589343
Conduitry <git@chor.date>
2020-04-05 18:04:03 -0400

    allow global return in scripts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
 	root: true,
 	extends: '@conduitry',
+	parserOptions: { sourceType: 'script', ecmaFeatures: { globalReturn: true } },
 	env: { node: false },
 	globals: { GM: 'readable', unsafeWindow: 'readable' },
 };
