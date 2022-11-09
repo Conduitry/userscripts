fe7a72dff4522a7d7da123f64b6c7e6286b04ea4
Conduitry <git@chor.date>
2022-11-09 10:49:00 -0500

    wikipedia: also hide inline banner

--- scripts/wikipedia.user.js
+++ scripts/wikipedia.user.js
@@ -4,9 +4,9 @@
 // @description A script to remove the fundraising banner.
 // @match https://*.wikipedia.org/*
 // @icon https://en.wikipedia.org/static/favicon/wikipedia.ico
-// @version 2022.08.24.113914
+// @version 2022.11.09.154900
 // ==/UserScript==
 
 const style = document.createElement('style');
-style.textContent = '#frb-main { display: none !important; }';
+style.textContent = '#frb-main, #frb-inline { display: none !important; }';
 document.body.appendChild(style);
