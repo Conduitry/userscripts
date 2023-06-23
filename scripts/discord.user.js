1aed3257c6dae07f9dc85a8ff1576e3ab4d18f36
Conduitry <git@chor.date>
2023-06-23 11:36:50 -0400

    discord: hide super reactions buttons

--- scripts/discord.user.js
+++ scripts/discord.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name Discord thing
 // @namespace https://chor.date
-// @description A script to remove role icons and server boost icons.
+// @description A script to remove role icons, server boost icons, and super reaction buttons.
 // @match https://discord.com/*
 // @icon https://discord.com/assets/847541504914fd33810e70a0ea73177e.ico
-// @version 2021.12.13.173452
+// @version 2023.06.23.153650
 // ==/UserScript==
 
 const style = document.createElement('style');
-style.textContent = '[class*=roleIcon], [class*=premiumIcon] { display:none; }';
+style.textContent = '[class*=roleIcon], [class*=premiumIcon], [aria-label="Add Super Reaction"] { display:none; }';
 document.head.appendChild(style);
